88ff8095773d1abe0b187de75cbd04ebbaea1c7b
Zdornex <zdornex@gmail.com>
2019-01-06 16:34:36 +0200

    connection

---
+++ src/HillelHomeTasks.java
@@ -0,0 +1,7 @@
+public class HillelHomeTasks {
+
+           public static void main(String[] args) {
+            System.out.println("just trying to install connection with GitHub via Idea");
+        }
+
+}
