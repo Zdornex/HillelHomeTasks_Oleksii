55498634572958a222acd70ee1b521e8c0ace00f
Zdornex <zdornex@gmail.com>
2019-01-14 23:42:06 +0200

    выполнил Task 3

---
+++ src/HomeTask3/EvenOddNumbers.java
@@ -0,0 +1,9 @@
+package HomeTask3;
+
+public class EvenOddNumbers {
+
+public static int EvenOdd (int a) {
+    int result = a%2;
+    return result;
+}
+}
---
+++ src/HomeTask3/Start.java
@@ -0,0 +1,11 @@
+package HomeTask3;
+
+import com.sun.javafx.geom.Crossings;
+
+public class Start {
+
+    public static void main(String[] args) {
+        int EvenOddresult = EvenOddNumbers.EvenOdd (6);
+        System.out.println(EvenOddresult);
+    }
+}
