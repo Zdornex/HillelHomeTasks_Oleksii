817e5fb8a05587710bc45ab7eec834ae413be39a
Zdornex <zdornex@gmail.com>
2019-01-14 23:12:55 +0200

    выполнил Task 2

---
+++ src/HomeTask2/Calculator.java
@@ -0,0 +1,29 @@
+package HomeTask2;
+
+public class Calculator {
+
+    public static double add(double a, double b, double c){
+        double result1 = a+b*(c/2);
+        return result1;
+        }
+
+        public static int square (int a, int b){
+        int a1 = a*a;
+        int b1 = b*b;
+        int result2 = a1+b1;
+        return result2;
+        }
+
+        public static double div (double a, double b, double c) {
+        double result3 = (a + b)/12 * c%4 + b;
+        return  result3;
+        }
+
+        public static double hop (double a, double b, double c) { //просто попробовал назвать метод произвольно "hop" - роде сработало)))))
+        double result4 = (a - b*c)/(a + b)%c;
+        return  result4;
+    }
+
+
+
+}
---
+++ src/HomeTask2/Start.java
@@ -0,0 +1,20 @@
+package HomeTask2;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        double addresult1 = Calculator.add(11, 35, 36);
+        System.out.println(addresult1);
+
+        int result2 = Calculator.square(2, 3);
+        System.out.println(result2);
+
+        double result3 = Calculator.div(2, 4, 7);
+        System.out.println(result3);
+
+        double result4 = Calculator.hop(8, 15, 54);
+        System.out.println(result4);
+
+    }
+}
