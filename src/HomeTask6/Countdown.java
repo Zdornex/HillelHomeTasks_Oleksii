55d0dc092408566a09bb24dd8de516fdf0714410
Zdornex <zdornex@gmail.com>
2019-01-29 21:09:09 +0200

    начало Task 6

---
+++ src/HomeTask6/Countdown.java
@@ -0,0 +1,19 @@
+package HomeTask6;
+
+public class Countdown {
+
+    public static void main(String[] args) {
+        int x= 10;
+
+        do {
+            if (x<=10) {
+                System.out.println("x=" + x);
+            }
+            x--;
+        }
+
+        while (x>0);
+
+    }
+
+}
